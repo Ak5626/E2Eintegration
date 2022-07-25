266c0e181a488ffaeaa7d8999ab671d2046c4eab
A.BASKARAN <A.BASKARAN@DESKTOP-6V86C2L.zurich.com>
2022-07-25 17:09:57 +0530

    Commit Automation

---
+++ E2ejenkinsframework/src/testcases/TC01.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class TC01 {
+	public WebDriver driver;
+	
+	@Test
+	public void init()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");				
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("name");
+		driver.findElement(By.id("pass")).sendKeys("pwd");
+				
+	}
+	
+}
